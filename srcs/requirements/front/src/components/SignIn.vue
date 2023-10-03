f98af88c0af64f1d675fee28171661f3f51928bf
Centiso <florian.sanna49@gmail.com>
2023-10-03 13:18:41 +0200

    Replaced axios with fetch.

--- srcs/requirements/front/src/components/SignIn.vue
+++ srcs/requirements/front/src/components/SignIn.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import Cookies from 'js-cookie';
-import axios from "axios";
 
 const userInfo = ref(null);
 
@@ -9,26 +8,36 @@ const code = new URL(window.location.href).searchParams.get("code");
 
 onMounted(async () => {
   if (code || Cookies.get('userLogin')) {
-    // Exchange the authorization code for an access token
     try {
-      const response = await axios.post("https://api.intra.42.fr/oauth/token", {
-        grant_type: "authorization_code",
-        client_id: import.meta.env.VITE_CLIENT_ID,
-        client_secret: import.meta.env.VITE_CLIENT_SECRET,
-        code: code,
-        redirect_uri: import.meta.env.VITE_REDIRECT_URI
+      const response = await fetch("https://api.intra.42.fr/oauth/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          grant_type: "authorization_code",
+          client_id: import.meta.env.VITE_CLIENT_ID,
+          client_secret: import.meta.env.VITE_CLIENT_SECRET,
+          code: code,
+          redirect_uri: import.meta.env.VITE_REDIRECT_URI
+        })
       });
-      const accessToken = response.data.access_token;
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      const accessToken = data.access_token;
 
-      // Use the access token to make authenticated requests
-      const userResponse = await axios.get("https://api.intra.42.fr/v2/me", {
+      const userResponse = await fetch("https://api.intra.42.fr/v2/me", {
         headers: {
           Authorization: `Bearer ${accessToken}`
         }
       });
-      const user = userResponse.data;
+      if (!userResponse.ok) {
+        throw new Error(`HTTP error! status: ${userResponse.status}`);
+      }
+      const user = await userResponse.json();
 
-      // Store the user information in the userInfo object
       userInfo.value = user;
       Cookies.set('userLogin', user.login, { expires: 1, secure: true, sameSite: 'Strict' });
     } catch (error) {
