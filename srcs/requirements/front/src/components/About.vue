2ca904ca254f66514830c8d3691fec05e1b8315f
Rart3x <contactramjattankeny@gmail.com>
2023-10-04 22:41:22 +0200

    Center profiles in About.vue

--- srcs/requirements/front/src/components/About.vue
+++ srcs/requirements/front/src/components/About.vue
@@ -1,59 +1,69 @@
 <script setup></script>
 
 <template>
-  <div class="profile_pictures_container">
-    <div class="profile_picture">
-      <img class="image" src="@/components/images/fsanna.jpg"/>
-      <div class="normal">
-        <div class="text">Fsanna</div>
+  <div class="container">
+    <div class="profile_pictures_container">
+      <div class="profile_picture">
+        <img class="image" src="@/components/images/fsanna.jpg"/>
+        <div class="normal">
+          <div class="text">Fsanna</div>
+        </div>
+        <div class="hover">
+          <div class="text">Barman</div>
+        </div>
       </div>
-      <div class="hover">
-        <div class="text">Barman</div>
+      <div class="profile_picture">
+        <img class="image" src="@/components/images/kramjatt.jpg"/>
+        <div class="normal">
+          <div class="text">Kramjatt</div>
+        </div>
+        <div class="hover">
+          <div class="text">Responsable Frigo</div>
+        </div>
       </div>
-    </div>
-    <div class="profile_picture">
-      <img class="image" src="@/components/images/kramjatt.jpg"/>
-      <div class="normal">
-        <div class="text">Kramjatt</div>
+      <div class="profile_picture">
+        <img class="image" src="@/components/images/nicarpen.jpg"/>
+        <div class="normal">
+          <div class="text">Nicarpen</div>
+        </div>
+        <div class="hover">
+          <div class="text">Dev sur Dofus</div>
+        </div>
       </div>
-      <div class="hover">
-        <div class="text">Responsable Frigo</div>
-      </div>
-    </div>
-    <div class="profile_picture">
-      <img class="image" src="@/components/images/nicarpen.jpg"/>
-      <div class="normal">
-        <div class="text">Nicarpen</div>
-      </div>
-      <div class="hover">
-        <div class="text">PL sur Dofus</div>
-      </div>
-    </div>
-    <div class="profile_picture">
-      <img class="image" src="@/components/images/dguillau.jpg"/>
-      <div class="normal">
-        <div class="text">Dguillau</div>
-      </div>
-      <div class="hover">
-        <div class="text">Dev jeu qui sait pas ce qu'il fait</div>
+      <div class="profile_picture">
+        <img class="image" src="@/components/images/dguillau.jpg"/>
+        <div class="normal">
+          <div class="text">Dguillau</div>
+        </div>
+        <div class="hover">
+          <div class="text">Dev jeu qui sait pas ce qu'il fait</div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 75vh;
+}
+
 .profile_pictures_container {
   display: flex;
-  justify-content: space-between;
-  align-items: center;
 }
+
 .profile_picture {
   position: relative;
   width: 20vw;
 }
+
 .profile_picture .image {
   width: 100%;
 }
+
 .profile_picture .text {
   color: #fff;
   font-size: 30px;
@@ -66,6 +76,7 @@
   transform: translate(-50%, -50%);
   width: 100%;
 }
+
 .profile_picture .hover {
   position: absolute;
   top: 0;
@@ -74,6 +85,7 @@
   opacity: 0;
   transition: .5s ease;
 }
+
 .profile_picture:hover .hover {
   opacity: 1;
 }
@@ -81,6 +93,7 @@
 .profile_picture .normal {
   transition: .5s ease;
 }
+
 .profile_picture:hover .normal {
   opacity: 0;
 }
