889ef587b6f6958c0d0844e6cf85386f4ec63afa
Keny Ramjattan <kramjatt@1H5.42angouleme.fr>
2023-10-04 14:50:01 +0200

    About modified

--- srcs/requirements/front/src/components/About.vue
+++ srcs/requirements/front/src/components/About.vue
@@ -1,102 +1,90 @@
 <script setup></script>
 
 <template>
-  <h1 class="item">About us</h1>
-  <div class="container">
-    <ol class="list_container">
-      <img src="@/components/images/kramjatt.jpg" class="profile_picture" />
-      <ul class="information">
-        <li>
-          <p>Login: Kramjatt</p>
-        </li>
-        <li>
-          <p>Role: Back-end</p>
-        </li>
-        <li>
-          <p>Explanation: TODO</p>
-        </li>
-        <li>
-          <p>Personal note:</p>
-        </li>
-      </ul>
-    </ol>
-    <ol class="list_container">
-      <img src="@/components/images/fsanna.jpg" class="profile_picture" />
-      <ul class="information">
-        <li>
-          <p>Login: Fsanna</p>
-        </li>
-        <li>
-          <p>Role: User management</p>
-        </li>
-        <li>
-          <p>Explanation: TODO</p>
-        </li>
-        <li>
-          <p>Personal note:</p>
-        </li>
-      </ul>
-    </ol>
-    <ol class="list_container">
-      <img src="@/components/images/dguillau.jpg" class="profile_picture" />
-      <ul class="information">
-        <li>
-          <p>Login: Dguillau</p>
-        </li>
-        <li>
-          <p>Role: Game developer</p>
-        </li>
-        <li>
-          <p>Explanation: TODO</p>
-        </li>
-        <li>
-          <p>Personal note:</p>
-        </li>
-      </ul>
-    </ol>
-    <ol class="list_container">
-      <img src="@/components/images/nicarpen.jpg" class="profile_picture" />
-      <ul class="information">
-        <li>
-          <p>Login: Nicarpen</p>
-        </li>
-        <li>
-          <p>Role: Front-end</p>
-        </li>
-        <li>
-          <p>Explanation: TODO</p>
-        </li>
-        <li>
-          <p>Personal note:</p>
-        </li>
-      </ul>
-    </ol>
+  <div class="profile_pictures_container">
+    <div class="profile_picture">
+      <img class="image" src="@/components/images/fsanna.jpg"/>
+      <div class="normal">
+        <div class="text">Fsanna</div>
+      </div>
+      <div class="hover">
+        <div class="text">Barman</div>
+      </div>
+    </div>
+    <div class="profile_picture">
+      <img class="image" src="@/components/images/kramjatt.jpg"/>
+      <div class="normal">
+        <div class="text">Kramjatt</div>
+      </div>
+      <div class="hover">
+        <div class="text">Responsable Frigo</div>
+      </div>
+    </div>
+    <div class="profile_picture">
+      <img class="image" src="@/components/images/nicarpen.jpg"/>
+      <div class="normal">
+        <div class="text">Nicarpen</div>
+      </div>
+      <div class="hover">
+        <div class="text">PL sur Dofus</div>
+      </div>
+    </div>
+    <div class="profile_picture">
+      <img class="image" src="@/components/images/dguillau.jpg"/>
+      <div class="normal">
+        <div class="text">Dguillau</div>
+      </div>
+      <div class="hover">
+        <div class="text">Dev jeu qui sait pas ce qu'il fait</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.item {
+.profile_pictures_container {
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
-  padding: 2vh;
 }
-
-.container {
-  display: flex;
-  flex-direction: column;
-  gap: 5vh;
-}
-
-.list_container {
-  display: flex;
-  gap: 5vw;
-}
-
 .profile_picture {
+  position: relative;
   width: 20vw;
 }
-.information {
-  list-style-type: none;
+.profile_picture .image {
+  width: 100%;
+}
+.profile_picture .text {
+  color: #fff;
+  font-size: 30px;
+  line-height: 1.5em;
+  text-shadow: 2px 2px 2px #000;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+}
+.profile_picture .hover {
+  position: absolute;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: .5s ease;
+}
+.profile_picture:hover .hover {
+  opacity: 1;
+}
+
+.profile_picture .normal {
+  transition: .5s ease;
+}
+.profile_picture:hover .normal {
+  opacity: 0;
+}
+.profile_picture .hover {
+  background-color: rgba(0,0,0,0.5);
 }
 </style>
