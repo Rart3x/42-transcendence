7791df3f421cd2db088a58e37f0499553897eafe
damienglld <damien17glld@outlook.com>
2023-10-18 14:31:34 +0200

    moved getter in the new get.call file

--- srcs/requirements/back/src/waiters/waiter.controller.ts
+++ srcs/requirements/back/src/waiters/waiter.controller.ts
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-import { BadRequestException, Body, Controller, Get, Post } from '@nestjs/common';
-=======
-import { Body, Controller, Post } from '@nestjs/common';
->>>>>>> 21e6fe95ab4ef746d866e9bd2bf9943066139f90
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Waiter } from '@prisma/client';
 import { WaiterService } from './waiter.service';
 import { Prisma } from '@prisma/client';
-import { warn } from 'console';
 
 @Controller('waiter')
 export class WaiterController {
@@ -16,13 +11,8 @@ export class WaiterController {
   async createWaiter(@Body() data: Prisma.WaiterCreateInput): Promise<Waiter> {
       return this.waiterService.createWaiter(data);
   }
-<<<<<<< HEAD
-
   @Get()
   async getFirstWaiter(): Promise<Waiter>{
     return this.waiterService.getFirstWaiter();
   }
 }
-=======
-}
->>>>>>> 21e6fe95ab4ef746d866e9bd2bf9943066139f90
--- srcs/requirements/front/src/components/api/get.call.ts
+++ srcs/requirements/front/src/components/api/get.call.ts
@@ -19,3 +19,26 @@ export async function getMessage() {
       console.error('error: sending GET request', error);
     }
 }
+
+export async function getClientFromWaitlist(){
+  try {
+    const response = await fetch("http://localhost:3000/waiter", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      const responseData = await response.json();
+      console.log(responseData);
+      return responseData;
+    }
+    else {
+      const errorText = await response.text();
+    }
+  } 
+  catch (error) {
+    console.error('error: sending GET request', error);
+  }
+}
+
