1382176b72bbe0daa92f9cc3d13f4f1463825445
Keny Ramjattan <kramjatt@1H7.42angouleme.fr>
2023-10-31 10:50:22 +0100

    Settings.vue

--- srcs/requirements/front/src/components/Settings.vue
+++ srcs/requirements/front/src/components/Settings.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import { updateUsername } from './api/post.call';
+    import { updateImage, updateUsername } from './api/post.call';
     
     const newUserName = ref("");
     const userName = ref("");
@@ -8,6 +8,16 @@
         await updateUsername(user.userName, newUserName.value);
         window.location.href = "/UpdateUsername";
     }
+
+    const uploadImage = async () => {
+
+        if (!selectedFile.value) {
+            alert('error: please select a file');
+            return;
+        }
+    await updateImage(user.userName, selectedFile.value);
+    window.location.href = "/Profile";
+}
 
 </script>
 
@@ -18,5 +28,11 @@
             <input type="text" id="newUserName" v-model="newUserName" :placeholder="userName" class="input input-bordered w-full max-w-xs" />
             <button class="btn">Send</button>
         </form>
+    </div>
+    <div class="divider"></div> 
+    <div class="form-control w-full max-w-xs">
+        <label>Please select your new profile image : </label>
+        <input type="file" class="file-input file-input-bordered w-full max-w-xs" />
+        <button class="btn" @click="uploadImage">Upload</button>
     </div>
 </template>
