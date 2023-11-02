3d9aac145572ebf042e97afe6a6bb2a6b68b2965
Rart3x <contactramjattankeny@gmail.com>
2023-11-02 15:03:31 +0100

    Verifications on channel creation, cant create a existent channel etc.. look code to understand

--- srcs/requirements/back/src/channel/channel.controller.ts
+++ srcs/requirements/back/src/channel/channel.controller.ts
@@ -8,10 +8,8 @@ export class ChannelController {
   constructor(private readonly channelService: ChannelService, private readonly prisma: PrismaService) {}
 
   @Post(':channelName')
-  async createChannel(@Param('channelName') channelName: string, @Body('userName') userName: string, @Body('invitedUserName') invitedUserName: string): Promise<Channel> {
+  async createChannel(@Body('channelName') channelName: string, @Body('userName') userName: string, @Body('invitedUserName') invitedUserName: string): Promise<Channel> {
     try {
-      console.log(userName);
-      console.log(invitedUserName);
       return this.channelService.createChannel(channelName, userName, invitedUserName);
     }
     catch (error) {
--- srcs/requirements/back/src/channel/channel.service.ts
+++ srcs/requirements/back/src/channel/channel.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Channel, Prisma } from '@prisma/client';
+import { Channel, User, Prisma } from '@prisma/client';
 import { UserService } from '../user/user.service';
 import { PrismaService } from '../prisma.service'
 
@@ -7,16 +7,58 @@ import { PrismaService } from '../prisma.service'
 export class ChannelService {
   constructor (private prisma: PrismaService, private userService: UserService) {}
 
+  async isUserAdminOfChannel(channel: Channel, user: User): Promise<Boolean> {
+    const channelUser = await this.prisma.channel.findFirst({
+      where: { channelAdmin: user.userId },
+    });
+    if(!channelUser)
+      return false;
+    return true;
+  }
+
+  async isUserMemberOfChannel(channel: Channel, user: User): Promise<Boolean> {
+    const channelUser = await this.prisma.channel.findFirst({
+      where: { channelId: channel.channelId },
+      select: {
+        channelUsers: {
+          where: { userId: user.userId } 
+        }
+      },
+    });
+    if(!channelUser)
+      return false;
+    return true;
+  }
+  
   async createChannel(channelName: string, userName: string, invitedUserName: string): Promise<Channel> {
     const user = await this.userService.getUserByUserName(userName);
     const invitedUser = await this.userService.getUserByUserName(invitedUserName);
-
-    console.log(user);
-    console.log(invitedUser);
-
+  
     if (!user || !invitedUser)
-      throw new Error("error: user or invited user not found");
-
+      console.error("error: user or invited user not found");
+  
+    const existingChannel = await this.getChannelByChannelName(channelName);
+  
+    if (existingChannel) {
+      if (await this.isUserAdminOfChannel(existingChannel, user))
+        console.error("error: user is already admin of this channel");
+  
+      if (await this.isUserMemberOfChannel(existingChannel, invitedUser)){
+        console.error("error: invited user is already member of this channel");
+        return null;
+      }
+  
+      await this.prisma.channel.update({
+        where: { channelId: existingChannel.channelId },
+        data: {
+          channelUsers: {
+            connect: { userId: invitedUser.userId },
+          },
+        },
+      });
+      return existingChannel;
+    }
+  
     const channel = await this.prisma.channel.create({
       data: {
         channelName: channelName,
@@ -29,6 +71,15 @@ export class ChannelService {
         },
       },
     });
+    return channel;
+  }
+
+  async getChannelByChannelName(channelName: string): Promise<Channel> {
+    const channel = await this.prisma.channel.findUnique({
+      where: { channelName: channelName },
+    });
+    if (!channel)
+      console.error("error: channel not found");
     return channel;
   }
 }
