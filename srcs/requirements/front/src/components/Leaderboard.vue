a7f00d469bc824ff7b8260f5bda73505fbd180b5
damienglld <damien17glld@outlook.com>
2023-11-19 22:01:28 +0100

    working on leaderboard

--- srcs/requirements/front/src/components/Leaderboard.vue
+++ srcs/requirements/front/src/components/Leaderboard.vue
@@ -1,24 +1,41 @@
 
-<script setup>
-  import { onMounted, ref} from "vue";
-  import { getAllUsers } from "./api/get.call";
-  
-  let users = ref(null);
-  onMounted(async () => {
-    users = await getAllUsers();
-    console.log(users);
-  });
+<script>
+import { onMounted, ref } from "vue";
+import { getAllUsers } from "./api/get.call";
+
+export default {
+ setup() {
+   const users = ref([]);
+
+   onMounted(async () => {
+     users.value = await getAllUsers();
+   });
+
+   return {
+     users
+   };
+ },
+ computed: {
+   sortUser: function() {
+     return this.users.sort().reverse();
+   }
+ },
+};
 
 </script>
 
 <template>
-    <div class="stats shadow">
-    <div class="stat">
-        <div v-for="user in users" :key="user">
-            <div class="stat-title">Username</div>
-            <div v-if="user" class="stat-value"> {{ user.userName }} </div>
+    <div v-for="(user,index) in sortUser" :key="user">
+      <div class="stats shadow">
+        <div class="stat">
+          <div class="stat-title"> Rank </div>
+          <div v-if="user" class="stat-value"> {{ index + 1 }} </div>
         </div>
-    </div>
+        <div class="stat">
+          <div class="stat-title"> Username </div>
+          <div class="stat-value"> {{ user.userName }} </div>
+        </div>
+      </div>
     </div>
 </template>
 
