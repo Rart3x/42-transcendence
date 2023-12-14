0ddd285001038ebc9a3485eb11476aad5d82d620
Rart3x <ramjattankeny971@gmail.com>
2023-12-14 23:54:39 +0100

    All dockers are up BUT application doesnt work when using back in front

--- srcs/requirements/back/wait-for-postgres.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-until PGPASSWORD="123" psql "postgresql://kramjatt:123@postgres:5432/PMU" -c '\q'; do
- >&2 echo "Postgres is unavailable - waiting"
- sleep 1
-done
-
->&2 echo "Postgres is up - executing command"
-exec "$@"
