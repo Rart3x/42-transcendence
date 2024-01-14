422b0d545954bb1f9bdd2e11a8ac961b74a06d84
damienglld <damien17glld@outlook.com>
2024-01-14 21:44:42 +0100

    fix script

--- change_hostname.sh
+++ change_hostname.sh
@@ -4,4 +4,4 @@ HOSTNAME=`env | grep SESSION_MANAGER | cut -d/ -f2 | cut -c -18`
 
 echo 'set website hostname to '$HOSTNAME''
 
-find . -type f -not -name "$0" -exec sed -i 's/2F2.42angouleme.fr/'$HOSTNAME'/g' {} +
+find . -type f -not -name "$0" -exec sed -i 's/localhost/'$HOSTNAME'/g' {} +
