4f5ccc1b2a1cf0f8b7a302420a6e76ab8f47a622
damienglld <114626618+damienglld@users.noreply.github.com>
2024-01-31 04:55:51 +0100

    c le grand jour omg

--- srcs/requirements/front/src/router/index.js
+++ srcs/requirements/front/src/router/index.js
@@ -116,15 +116,37 @@ router.beforeEach(async (to, from, next) => {
   if (path === '/checkPass/:channelName')
     comingFromCheckPass = true;
 
-  if (path == '/2fa' && user && user.A2F == true && user.A2FValid == false) {
-    next(true);
-  } else if (user && user.A2FValid == false && user.A2F == true && to.path != '/2fa') {
-    next('/2fa');
-  } else if (user && user.A2F == false){
-    next(true);
-  } else if (!user && to.path == '/'){
+  console.log(to.path);
+  console.log(from.path);
+
+  if (user)
+    console.log(user.A2FValid);
+
+  if (to.path == '/2fa' && user && user.A2F == true && user.A2FValid == false) {
+    console.log('here');
     next(true);
   }
+  else if (user && user.A2FValid == false && user.A2F == true && to.path != '/2fa') {
+    console.log('there');
+    next('/2fa');
+  }
+  else{
+    console.log('everywhere');
+    next(true);
+  }
+  // if (path == '/2fa' && user && user.A2F == true && user.A2FValid == false) {
+  //   console.log('here');
+  //   next(true);
+  // } else if (user && user.A2FValid == false && user.A2F == true && to.path != '/2fa') {
+  //   console.log('there');
+  //   next('/2fa');
+  // } else if (user && user.A2F == false){
+  //   console.log('everywhere');
+  //   next(true);
+  // } else if (!user && to.path == '/'){
+  //   console.log('nowhere');
+  //   next(true);
+  // }
 });
 
 export default router;
